6a9f8a4af567031171143d5e7e922e1f4eb6d8ce
Malabeadman <deangelo.lee19@gmail.com>
2022-11-04 19:30:29 -0400

    First

---
+++ BillApp/BillApp/BillAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillAppApp.swift
+//  BillApp
+//
+//  Created by Deangelo Stewart on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BillAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
